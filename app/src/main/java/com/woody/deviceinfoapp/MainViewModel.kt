e18fb3303f03a416039408229241ab6f1316d486
SongYungi <syg1138@gmail.com>
2020-06-23 17:49:33 +0900

    [Default] OS 버전 정보 가져오기

--- app/src/main/java/com/woody/deviceinfoapp/MainViewModel.kt
+++ app/src/main/java/com/woody/deviceinfoapp/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.woody.deviceinfoapp
 
 import android.os.Build
+import android.os.Build.VERSION_CODES
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -14,6 +15,37 @@ class MainViewModel : ViewModel() {
     val deviceModel: MutableLiveData<CharSequence> by lazy {
         MutableLiveData<CharSequence>().apply {
             value = Build.MODEL
+        }
+    }
+
+    val androidVersionNumber: MutableLiveData<CharSequence> by lazy {
+        MutableLiveData<CharSequence>().apply {
+            val sdkInt = Build.VERSION.SDK_INT.toString()
+            var sdkName : String? = null;
+            val fields =
+                VERSION_CODES::class.java.fields
+            for (field in fields) {
+                val fieldName = field.name
+                var fieldValue = -1
+                try {
+                    fieldValue = field.getInt(Any())
+                } catch (e: IllegalArgumentException) {
+                    e.printStackTrace()
+                } catch (e: IllegalAccessException) {
+                    e.printStackTrace()
+                } catch (e: NullPointerException) {
+                    e.printStackTrace()
+                }
+                if (fieldValue == Build.VERSION.SDK_INT) {
+                    sdkName = fieldName
+                }
+            }
+
+            value = if (sdkName != null) {
+                "$sdkInt($sdkName)"
+            } else {
+                sdkInt
+            }
         }
     }
 
